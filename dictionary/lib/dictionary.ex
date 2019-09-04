1be38f25efeb6cee36c6ee923a5ee0453e03acf3
Eugene Osadchyi <eugene.osadchyi@gmail.com>
2019-09-04 17:51:03 +0300

    Initialize repository

---
+++ dictionary/lib/dictionary.ex
@@ -0,0 +1,19 @@
+defmodule Dictionary do
+  def random_word() do
+    word_list()
+    |> Enum.random()
+  end
+
+  def word_list do
+    "../assets/words.txt"
+    |> Path.expand(__DIR__)
+    |> File.read!()
+    |> String.split(~r/\n/)
+  end
+
+  def swap({ a, b }), do: { b, a }
+
+  def eq(a, a), do: true
+
+  def eq(_, _), do: false
+end
---
+++ dictionary/run.sh
@@ -0,0 +1,1 @@
+docker run -it --rm -v $(pwd):/src -w /src elixir bash
