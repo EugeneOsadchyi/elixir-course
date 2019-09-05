41d01e2d70d5c0fb79620a6034f8cde3231b3f2d
Eugene Osadchyi <eugene.osadchyi@gmail.com>
2019-09-05 16:21:05 +0300

    Added Hangman API implementation

--- hangman/test/game_test.exs
+++ hangman/test/game_test.exs
@@ -17,53 +17,78 @@ defmodule GameTest do
   test "state isn't changed for :won or :lost game" do
     for state <- [ :won, :lost ] do
       game = Game.new_game() |> Map.put(:game_state, state)
-      assert { ^game, _ } = Game.make_move(game, "x")
+      assert ^game = Game.make_move(game, "x")
     end
   end
 
   test "first occurence of letter is not already used" do
     game = Game.new_game()
-    { game, _tally } = Game.make_move(game, "x")
+    game = Game.make_move(game, "x")
     assert game.game_state != :already_used
   end
 
   test "second occurence of letter is not already used" do
     game = Game.new_game()
-    { game, _tally } = Game.make_move(game, "x")
+    game = Game.make_move(game, "x")
     assert game.game_state != :already_used
 
-    { game, _tally } = Game.make_move(game, "x")
+    game = Game.make_move(game, "x")
     assert game.game_state == :already_used
   end
 
   test "a good guess is recognised" do
     game = Game.new_game("wibble")
-    { game, _tally } = Game.make_move(game, "w")
+    game = Game.make_move(game, "w")
     assert game.game_state == :good_guess
     assert game.turns_left == 7
   end
 
   test "a guessed word is a won game" do
+    moves = [
+      {"w", :good_guess, 7},
+      {"i", :good_guess, 7},
+      {"b", :good_guess, 7},
+      {"b", :already_used, 7},
+      {"l", :good_guess, 7},
+      {"e", :won, 7}
+    ]
+
+    assert_moves("wibble", moves)
+  end
+
+  test "bad guess is recognised" do
     game = Game.new_game("wibble")
+    game = Game.make_move(game, "x")
+    assert game.game_state == :bad_guess
+    assert game.turns_left == 6
+  end
 
-    { game, _tally } = Game.make_move(game, "w")
-    assert game.game_state == :good_guess
-    assert game.turns_left == 7
+  test "lost game is recognised" do
+    moves = [
+      {"a", :bad_guess, 6},
+      {"b", :bad_guess, 5},
+      {"c", :bad_guess, 4},
+      {"d", :bad_guess, 3},
+      {"e", :bad_guess, 2},
+      {"f", :bad_guess, 1},
+      {"g", :lost, 0}
+    ]
 
-    { game, _tally } = Game.make_move(game, "i")
-    assert game.game_state == :good_guess
-    assert game.turns_left == 7
+    assert_moves("w", moves)
+  end
 
-    { game, _tally } = Game.make_move(game, "b")
-    assert game.game_state == :good_guess
-    assert game.turns_left == 7
+  def assert_moves(word, moves) do
+    game = Game.new_game(word)
 
-    { game, _tally } = Game.make_move(game, "l")
-    assert game.game_state == :good_guess
-    assert game.turns_left == 7
+    Enum.reduce(moves, game, fn ({guess, state, turns_left}, game) ->
+      game = Game.make_move(game, guess)
 
-    { game, _tally } = Game.make_move(game, "e")
-    assert game.game_state == :won
-    assert game.turns_left == 7
+      assert %{
+        game_state: ^state,
+        turns_left: ^turns_left
+      } = game
+
+      game
+    end)
   end
 end
